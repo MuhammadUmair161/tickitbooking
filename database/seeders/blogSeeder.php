febeb3734c850e3b16aa1dd93d8cdef54c9ed694
Muhammad Umair <muhammadumair6922@gmail.com>
2022-07-10 20:51:26 +0500

    seeder for movie & blog

---
+++ database/seeders/blogSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Blog;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class blogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $faker = Faker::create();
+        // $blog = new Blog;
+        // $blog->title = $faker->title;
+        // $blog->sub_title = $faker->title;
+        // $blog->blog = Str::random(100);
+        // $blog->user_id = 1;
+        // // $blog->image = 'assets\images\blog\blog01.jpg';
+        // $blog->save();
+
+    }
+}
